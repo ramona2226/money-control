e328c695607876b69dac5de1474ef0c930b47f18
Ramona2226 <brisc.ramona22@yahoo.com>
2020-10-23 23:11:04 +0300

    added integrating tests for createUser

--- src/main/java/org/fasttrackit/moneycontrol/service/UserService.java
+++ src/main/java/org/fasttrackit/moneycontrol/service/UserService.java
@@ -2,6 +2,7 @@ package org.fasttrackit.moneycontrol.service;
 
 import org.fasttrackit.moneycontrol.domain.User;
 import org.fasttrackit.moneycontrol.persistance.UserRepository;
+import org.fasttrackit.moneycontrol.transfer.SaveUserRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,7 +27,7 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
-public User createUser() {
+public User createUser(SaveUserRequest request) {
     LOGGER.info("Creating User: {}" , request);
 
     User user = new User();
