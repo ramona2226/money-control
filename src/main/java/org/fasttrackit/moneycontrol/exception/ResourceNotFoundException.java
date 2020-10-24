81ea78c45e1c958680c0cb29ba92824d713c9084
Ramona2226 <brisc.ramona22@yahoo.com>
2020-10-24 18:20:27 +0300

    added getUser method in user service

---
+++ src/main/java/org/fasttrackit/moneycontrol/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package org.fasttrackit.moneycontrol.exception;
+
+public class ResourceNotFoundException  extends RuntimeException{
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
--- src/main/java/org/fasttrackit/moneycontrol/service/UserService.java
+++ src/main/java/org/fasttrackit/moneycontrol/service/UserService.java
@@ -1,6 +1,7 @@
 package org.fasttrackit.moneycontrol.service;
 
 import org.fasttrackit.moneycontrol.domain.User;
+import org.fasttrackit.moneycontrol.exception.ResourceNotFoundException;
 import org.fasttrackit.moneycontrol.persistance.UserRepository;
 import org.fasttrackit.moneycontrol.transfer.SaveUserRequest;
 import org.slf4j.Logger;
@@ -8,38 +9,46 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class UserService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(UserService.class);
 
 
-
-
-
     //Inversion of Control (IoC)
     private final UserRepository userRepository;
 
 
-//Dependency injection
+    //Dependency injection
     @Autowired
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
-public User createUser(SaveUserRequest request) {
-    LOGGER.info("Creating User: {}" , request);
+    public User createUser(SaveUserRequest request) {
+        LOGGER.info("Creating User: {}", request);
 
-    User user = new User();
-    user.setFirstName(request.getFirstName());
-    user.setLastName(request.getLastName());
+        User user = new User();
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
 
-     return userRepository.save(user);
-
+        return userRepository.save(user);
 
 
     }
 
+    public User getUser(long id) {
+
+        LOGGER.info("Retrieving user{}", id);
+        Optional<User> userOptional = userRepository.findById(id);
+        if (userOptional.isPresent()) {
+            return userOptional.get();
+        } else {
+            throw new ResourceNotFoundException("User " + id + "does not exist");
 
 
+        }
+    }
 }
