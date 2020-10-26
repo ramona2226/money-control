ff8eabcb1f93739b45d98f4642eb5db528584eb2
Ramona2226 <brisc.ramona22@yahoo.com>
2020-10-26 16:47:14 +0200

    studied queries method in spring boot and JPQL SYNTAX

--- src/main/java/org/fasttrackit/moneycontrol/persistance/UserRepository.java
+++ src/main/java/org/fasttrackit/moneycontrol/persistance/UserRepository.java
@@ -4,14 +4,20 @@ import org.fasttrackit.moneycontrol.domain.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
 
-    Page<User> findByFirstNameContains (String partialFirstName, Pageable pageable);
+    Page<User> findByFirstNameContains(String partialFirstName, Pageable pageable);
 
     Page<User> findByLastNameContains(String partialLastName, Pageable pageable);
 
-    Page<User> findByFirstNameContainsAndLastNameContains (String partialFirstName,  String partialLastName, Pageable pageable);
-
+    //dintr-o metoda custom query
+    //JPQL JAVA PERSISTANCE QUERY LEVEL
+    @Query(value = "SELECT user FROM User user" +
+            " Where (:partialFirstName IS NULL OR user.firstName LIKE %:partialFirstName%) " +
+            " AND (:partialLastName IS NULL OR user.lastName LIKE %:partialFirstName%)")
+    Page<User> findByOptionalCriteria(String partialFirstName, String partialLastName, Pageable pageable);
 }
+
